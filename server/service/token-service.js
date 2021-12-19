8d248834bca6c98e9bfebc306eedc3a3282908df
vika--babenko <vika.babenko.18.09.02@gmail.com>
2021-12-19 22:17:20 +0200

    change token-service.js and index.js

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const tokenModel = require('../models/token-model');
 class TokenService{
     generateTokens(payload){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn:'15m'});
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn:'30s'});
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn:'30d'});
         return {
             accessToken, refreshToken
