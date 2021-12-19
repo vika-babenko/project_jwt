c4f73603adad1cb2cbbc6f7a13497343729eeaeb
vika--babenko <vika.babenko.18.09.02@gmail.com>
2021-12-19 21:47:45 +0200

    add functions for logout and login

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,7 +1,14 @@
-const userService = require('../service/user-service')
+const userService = require('../service/user-service');
+const {validationResult} = require('express-validator');
+const ApiError = require('../exceptions/api-error');
+
 class UserController{
     async registration(req, res, next){
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+            }
             const {email, password} = req.body;
             const userData = await userService.registration(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
@@ -14,16 +21,24 @@ class UserController{
     }
     async login(req, res, next){
         try {
+            const {email, password} = req.body;
+            const userData = await userService.login(email, password);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData);
 
         } catch (e) {
+            next(e);
 
         }
     }
     async logout(req, res, next){
         try {
-
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token);
         } catch (e) {
-
+            next(e);
         }
     }
     async activate(req, res, next){
@@ -31,16 +46,14 @@ class UserController{
             const activationLink = req.params.link;
             await userService.activate(activationLink);
             return res.redirect(process.env.CLIENT_URL);
-
         } catch (e) {
             next(e);
         }
     }
     async refresh(req, res, next){
         try {
-
         } catch (e) {
-
+            next(e)
         }
     }
     async getUsers(req, res, next){
@@ -48,7 +61,7 @@ class UserController{
             res.json(['123', '456']);
 
         } catch (e) {
-
+            next(e)
         }
     }
 }
