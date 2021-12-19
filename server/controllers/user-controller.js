9cb49a4c83a6b37e335e6637c8ac1829729d08a6
vika--babenko <vika.babenko.18.09.02@gmail.com>
2021-12-19 21:50:18 +0200

    add functions for logout and login

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -50,10 +50,14 @@ class UserController{
             next(e);
         }
     }
-    async refresh(req, res, next){
+    async refresh(req, res, next) {
         try {
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData);
         } catch (e) {
-            next(e)
+            next(e);
         }
     }
     async getUsers(req, res, next){
