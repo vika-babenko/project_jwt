10c4e3b088545a0ea8cb87472ba33863a1e583b3
vika--babenko <vika.babenko.18.09.02@gmail.com>
2021-12-19 21:07:41 +0200

    First commit

---
+++ server/service/user-service.js
@@ -0,0 +1,35 @@
+const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto=require('../dtos/user-dto')
+
+class UserService{
+    async registration(email, password){
+        const candidate = await UserModel.findOne({email});
+        if (candidate){
+            throw new Error('Користувач з такою адресою ${email} вже існує')
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return{...tokens, user: userDto}
+    }
+    async activate(activationLink){
+        const user = await UserModel.findOne({activationLink})
+        if(!user){
+            throw new Error("Некорректная ссылка активации")
+        }
+        user.isActivated = true;
+        await user.save();
+    }
+}
+
+module.exports = new UserService();
